8ebef658cc7adc8dc12bd0332f6ee4f1a8643919
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-09-10 16:02:26 +0100

    Added music and butt

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,39 @@
 #include <SFML/Graphics.hpp>
-
+// Library needed for playing music and sound effects
+#include <SFML/Audio.hpp>
 
 // entry point for the program	
 int main()
 {
+	//**************************
+	//********game setup********
+	//**************************
 	// declare variable for display window
 	sf::RenderWindow gamewindow;
 	gamewindow.create(sf::VideoMode::getDesktopMode(), "Button Masher", 
 		sf::Style::Titlebar | sf::Style::Close);
 
-	// game loop
+	//create button sprite
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
+
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+	
+	//set sprite coords
+	buttonSprite.setPosition(
+		gamewindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+		gamewindow.getSize().y / 2 - buttonTexture.getSize().y / 2
+	);
+
+	// create music object
+	sf::Music gameMusic;
+	gameMusic.openFromFile("audio/music.ogg");
+	gameMusic.play();
+
+	//*************************
+	//******* game loop *******
+	//*************************
 	while (gamewindow.isOpen())
 	{
 		// TODO: Check for input
@@ -29,7 +53,13 @@ int main()
 		// TODO: update game state
 
 		// TODO: Draw Graphics
+		gamewindow.clear(sf::Color::Transparent);
 
+		//draw everything
+		gamewindow.draw(buttonSprite);
+
+		//display window content
+		gamewindow.display();
 
 	}
 
